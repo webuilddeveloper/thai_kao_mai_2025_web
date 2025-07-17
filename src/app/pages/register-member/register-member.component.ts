ba7aadf257f33035021ba444b97c6247bc66fac4
webuilddeveloper <106293550+webuilddeveloper@users.noreply.github.com>
2025-07-17 16:46:53 +0700

    update register member

---
+++ src/app/pages/register-member/register-member.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ServiceProvider } from 'src/app/shared/service-provider.service';
+
+@Component({
+  selector: 'app-register-member',
+  templateUrl: './register-member.component.html',
+  styleUrls: ['./register-member.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class RegisterMemberComponent implements OnInit {
+  constructor(
+    private serviceProvider: ServiceProvider,
+    public translate: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+
+  }
+
+}
