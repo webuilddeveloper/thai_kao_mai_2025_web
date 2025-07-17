f45d3349346c207f52232950d0643550b44a934d
Porntavee Chantanit <pokub@hotmail.com>
2025-07-17 16:52:09 +0700

    อัพเดท หน้าจอบริจาค

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -32,4 +32,28 @@ export class DonateComponent {
   selectMethod(method: string) {
     this.selectedMethod = method;
   }
+
+
+  presetAmounts: number[] = [2000, 1000, 500];
+  selectedAmount: number | null = 1000; // ค่าเริ่มต้น
+  customAmount: number | null = null;
+
+  selectAmount(amount: number | null) {
+    this.selectedAmount = amount;
+    if (amount !== null) {
+      this.customAmount = null;
+    }
+  }
+
+  step: number = 1;
+
+  goToStep(stepNumber: number) {
+    this.step = stepNumber;
+  }
+
+  phone: string = '';
+  email: string = '';
+
+
+  qrImageUrl: string = './assets/img/QR2.png'; // เปลี่ยนเป็น dynamic URL ได้ในอนาคต
 }
