a672300bd42b3e7e556b254ea5bd988204af3827
Porntavee Chantanit <pokub@hotmail.com>
2025-07-17 15:36:45 +0700

    อัพเดท หน้าจอบริจาค

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -9,14 +9,11 @@ import * as AOS from 'aos';
   styleUrls: ['./donate.component.scss']
 })
 export class DonateComponent {
-newsList: any[] = [];
-  newsHighlight: any[] = [];
+  selectedMethod: string = 'qr'; // ตั้งค่าเริ่มต้น
 
   constructor(private serviceProvider: ServiceProvider, public translate: TranslateService) { }
 
-
   ngOnInit(): void {
-
     AOS.init({
       duration: 800,
       once: false,
@@ -25,55 +22,11 @@ newsList: any[] = [];
     });
 
     setTimeout(() => {
-      AOS.refresh(); 
+      AOS.refresh();
     }, 100);
-
-    const url = 'm/news/read';
-
-    const body = {
-      skip: 0,
-      limit: 5,
-      permission: "20250617105043-193-133",
-      keySearch: "",
-      center: null,
-      imageUrlCreateBy: "https://khubdeedlt.we-builds.com/khubdeedlt-document/images/personal/personal_250438651.png",
-      updateBy: "admincms",
-      lv0: "",
-      lv1: "",
-      lv2: "",
-      lv3: "",
-      lv4: "",
-      organization: [
-        {
-          title: "super admin",
-          lv0: "",
-          lv1: "",
-          lv2: "",
-          lv3: "",
-          lv4: "",
-          lv5: "",
-          status: "A"
-        }
-      ]
-    };
-
-    this.serviceProvider.post(url, body).subscribe((data) => {
-      var model: any = {}
-      model = data;
-      this.newsList = model.objectData
-      this.newsHighlight = model.objectData;
-    });
   }
 
-  formatThaiDate(input: string): string {
-    const year = +input.substring(0, 4);
-    const month = +input.substring(4, 6);
-    const day = +input.substring(6, 8);
-    const date = new Date(year, month - 1, day);
-    const thaiMonths = [
-      "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน",
-      "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
-    ];
-    return `${date.getDate()} ${thaiMonths[date.getMonth()]} ${date.getFullYear()}`;
+  selectMethod(method: string) {
+    this.selectedMethod = method;
   }
 }
