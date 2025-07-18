592a64efc06e8f764750c4a461c9a1097dab9470
webuild <>
2025-07-19 00:49:39 +0700

    update

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ServiceProvider } from 'src/app/shared/service-provider.service';
 import * as AOS from 'aos';
 import { Router } from '@angular/router';
+import { gsap } from 'gsap';
+import { TextPlugin } from 'gsap/TextPlugin';
 
 @Component({
   selector: 'app-donate',
@@ -16,13 +18,20 @@ export class DonateComponent {
   formType: string = 'person'; // ค่าเริ่มต้นคือบุคคล
   firstName: string = '';
   lastName: string = '';
-   targetAmount = 1000000;
+  targetAmount = 1000000;
   currentAmount = 0;
-  targetDisplayAmount = 300000; 
+  targetDisplayAmount = 435340;
   percentage = 0;
+  finalNumber = '134';
+  digits = Array.from(this.finalNumber);
+  @ViewChildren('digitRef') digitRefs!: QueryList<ElementRef>;
+  @ViewChildren('underlineRef') underlineRefs!: QueryList<ElementRef>;
   constructor(private serviceProvider: ServiceProvider, public translate: TranslateService, private router: Router,) { }
 
   ngOnInit(): void {
+    gsap.registerPlugin(TextPlugin);
+
+    this.readMember();
     this.donateTotal();
     AOS.init({
       duration: 800,
@@ -74,7 +83,7 @@ export class DonateComponent {
 
 
   donateTotal() {
-     const duration = 2000;
+    const duration = 2000;
     const frameRate = 60;
     const totalFrames = Math.round(duration / (1000 / frameRate));
     let frame = 0;
@@ -93,5 +102,60 @@ export class DonateComponent {
 
     requestAnimationFrame(animate);
   }
+
+  readMember() {
+    setTimeout(() => {
+
+      const targetNumber = 134;
+      const digitsStr = targetNumber.toString().padStart(3, '0');
+      this.digits = digitsStr.split('');
+
+      const delayPerDigit = 0.2;
+      const randomCount = 30;
+      const singleDigitDuration = 0.05;
+
+      const digitArray = this.digitRefs?.toArray().reverse();
+      const underlineArray = this.underlineRefs?.toArray().reverse();
+
+      if (!digitArray || !underlineArray || digitArray.length === 0) {
+        console.error('Digit references not ready yet.');
+        return;
+      }
+
+
+      digitArray.forEach((digitEl, i) => {
+        const element = digitEl.nativeElement;
+        const underline = underlineArray[i].nativeElement;
+
+        const reversedIndex = this.digits.length - 1 - i;
+        const finalValue = this.digits[reversedIndex];
+
+        const tl = gsap.timeline({ delay: i * delayPerDigit });
+
+        for (let j = 0; j < randomCount; j++) {
+          tl.to(element, {
+            text: { value: Math.floor(Math.random() * 10).toString() },
+            duration: singleDigitDuration,
+            ease: 'none'
+          });
+        }
+
+        tl.to(element, {
+          text: { value: finalValue },
+          duration: 0.2,
+          ease: 'power2.out',
+          onComplete: () => {
+            gsap.to(underline, {
+              transformOrigin: 'right',
+              scaleX: 1,
+              duration: 0.4,
+              ease: 'power2.out'
+            });
+          }
+        });
+      });
+    }, 0);
+  }
 }
+
 
