167e4c3ed49683130172416ebb7a62df7391cdaa
webuilddeveloper <106293550+webuilddeveloper@users.noreply.github.com>
2025-07-29 15:59:49 +0700

    update suggestion

--- src/app/pages/suggestion/suggestion.component.ts
+++ src/app/pages/suggestion/suggestion.component.ts
@@ -5,18 +5,20 @@ import { ServiceProvider } from 'src/app/shared/service-provider.service';
 @Component({
   selector: 'app-suggestion',
   templateUrl: './suggestion.component.html',
-  styleUrls: ['./suggestion.component.scss']
+  styleUrls: ['./suggestion.component.scss'],
 })
 export class SuggestionComponent {
   isModalOpen = false;
-  deviceSize='';
+  deviceSize = '';
+  model: any = { category: '', email: '', description: '',status:'N' };
+  modelCategory: any = [];
   constructor(
     private serviceProvider: ServiceProvider,
     public translate: TranslateService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-
+    this.callReadCategory();
   }
 
   openModal() {
@@ -25,5 +27,26 @@ export class SuggestionComponent {
 
   closeModal() {
     this.isModalOpen = false;
+  }
+
+  callReadCategory() {
+    this.serviceProvider
+      .post('m/suggestion/category/read', {})
+      .subscribe((res) => {
+        let data: any = {};
+        data = res;
+        this.modelCategory = data.objectData;
+      });
+  }
+
+  sendApi() {
+    if (this.model.category == '') return;
+    this.serviceProvider
+      .post('suggestion/create', this.model)
+      .subscribe((res) => {
+        let data: any = {};
+        data = res;
+        this.openModal();
+      });
   }
 }
